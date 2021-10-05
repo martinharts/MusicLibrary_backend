4e745ef2dd59f9e72a6840d56b410cfc613e0807
martinharts <89169036+martinharts@users.noreply.github.com>
2021-10-04 21:48:31 -0400

    added get methods for all songs and songs by id. verified operation through postman

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const repoContext = require('./repository/repository-wrapper');
 
 app.listen(3000, function(){
     console.log("Server started. listening on port 3000.");
 });
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+app.get('/api/songs/:id', (req,res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
